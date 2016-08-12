d18e5012aab95edd4cfaa847438d686b1e0b00df
Kyle Vassella <KyleVassella@gmail.com>
2016-08-11 21:38:09 -0500

    revised pagination, syntax errors, jQuery file path
    Removed single page button if 10 or less students match with search
    (page button not needed in this case), revised indentations, missing
    and un-needed semi-colons, fixed jQuery file path, was humbled.

--- js/main.js
+++ js/main.js
@@ -2,57 +2,56 @@ var allStudents = $('.student-item').toArray();
 console.log(allStudents);
 var pageSize = 10;
 var searchText;
-var pageNumber;
 var currentPage;
 
-  //  initial state of the page
+  // initial state of the page
 var onPageLoad = function() {
-$(allStudents).hide();
-$('.student-item:nth-child(-n+10)').show(); //shows only the first 10 students
-currentPage = 1;
+  $(allStudents).hide();
+  $('.student-item:nth-child(-n+10)').show(); //shows only the first 10 students
+  currentPage = 1;
 };
 
-//  custom .hide() and .show() event handlers for showArray2() animations
+  // custom .hide() and .show() event handlers for showArray2() animations
 var fadeOut = function() {
   $(this).animate({opacity: '0'}, '3s');
-}
+};
 
 var fadeIn = function() {
   $(this).animate({opacity: '1'}, '.3s');
-}
+};
 
 
-      // displays students (10 per page) whose name or email contains what user types into input
-    var showArray = function() {
-      if (allStudents.length > 0) {  
-      var tenPerPage = $(allStudents).hide().slice((currentPage - 1) * pageSize, ((currentPage - 1) * pageSize) + pageSize).show();
-      } else {
-        var sorryMessage = '<li class="sorry">Sorry, we were unable to find a result</li>';
-        $('.student-list').append(sorryMessage);
-      }
-    }
+  // displays students (10 per page) whose name or email contains what user types into input
+var showArray = function() {
+  if (allStudents.length > 0) {  
+    $(allStudents).hide().slice((currentPage - 1) * pageSize, ((currentPage - 1) * pageSize) + pageSize).show(); // shows ten students per page
+    } else {
+    var sorryMessage = '<li class="sorry">Sorry, we were unable to find a result</li>';
+    $('.student-list').append(sorryMessage);
+  }
+};
     
     
-    // a second showArray method for use with the pagination buttons (so that animations only happen when pagination buttons are clicked, not when the search bar is used).
-    var showArray2 = function() {
-        var tenPerPage2 = $(allStudents).hide(fadeOut).slice((currentPage - 1) * pageSize, ((currentPage - 1) * pageSize) + pageSize).show(fadeIn);
-    }
+  // a second showArray method for use with the pagination buttons (so that animations only happen when pagination buttons are clicked, not when the search bar is used).
+var showArray2 = function() {
+  $(allStudents).hide(fadeOut).slice((currentPage - 1) * pageSize, ((currentPage - 1) * pageSize) + pageSize).show(fadeIn); //  shows ten students per page
+};
 
 
-    // adds dynamic page buttons based on number of search results
-    var dynamicPageButtons = function() {
-      $('.pagination ul li').remove();
-        var pageNumber = Math.ceil( allStudents.length / pageSize)
-        for (var i = 0; i < pageNumber; i++) {
-          $('.pagination ul').append('<li><a href ="#">' + (i+1) + '</a></li>')
-        }
-      $('.pagination ul li a').eq(0).addClass('active');
-    }
+  // adds dynamic page buttons based on number of search results
+var dynamicPageButtons = function() {
+  $('.pagination ul li').remove();
+  var pageNumber = Math.ceil( allStudents.length / pageSize);
+  for (var i = 0; i < pageNumber; i++) {
+    $('.pagination ul').append('<li><a href ="#">' + (i+1) + '</a></li>');
+  }
+  $('.pagination ul li a').eq(0).addClass('active');
+};
 
     
-  //  appends search bar
+  // appends search bar
 $('.page-header').append('<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>');
-  //  appends pagination button div
+  // appends pagination button div
 $('.student-list').after('<div class="pagination"><ul></ul></div>');
 
   //sets beginning state of page (hides all students, shows 10)
@@ -62,25 +61,30 @@ dynamicPageButtons();
 
     
     //  dynamic search function
-    var search = $('.page-header input').keyup(function() {
-      searchText = $('.page-header input').val().toLowerCase();
-      console.log(searchText);
-      $(allStudents).animate({opacity: '1'}, '1');      //  crucial reset of the visibility state of allStudents (otherwise search function visually breaks after clicking a pagination button)
-      $(allStudents).hide();
-      $('.sorry').remove();   // removes any 'Sorry, we were unable to find a result' messages
-      allStudents = [];   // empties the allStudents array, making room for only matched students to be included in array
-      $('.student-item').each(function () {
-        var namesAndEmails = $(this).find('.student-details').text().toLowerCase();
-        if ( namesAndEmails.indexOf(searchText) !== -1) {
-          var pushed = $(this)[0];  
-          allStudents.push(pushed);
-          console.log(allStudents);
-          };
-        })
-      currentPage = 1;
-      showArray();
-      dynamicPageButtons();
-      })
+$('.page-header input').keyup(function() {
+  searchText = $('.page-header input').val().toLowerCase();
+  console.log(searchText);
+  $(allStudents).animate({opacity: '1'}, '1');      //  crucial reset of the visibility state of allStudents (otherwise search function visually breaks after clicking a pagination button)
+  $(allStudents).hide();
+  $('.sorry').remove();   // removes any 'Sorry, we were unable to find a result' messages
+  allStudents = [];   // empties the allStudents array, making room for only matched students to be included in array
+  $('.student-item').each(function () {
+    var namesAndEmails = $(this).find('.student-details').text().toLowerCase();
+    if ( namesAndEmails.indexOf(searchText) !== -1) {
+      var pushed = $(this)[0];  
+      allStudents.push(pushed);
+      console.log(allStudents);
+    }
+  });
+  currentPage = 1;
+  showArray();
+  dynamicPageButtons();
+  if (allStudents.length <11) {
+    $('.pagination ul').hide()
+  } else {
+    $('.pagination ul').show()
+  }
+});
 
 
 
